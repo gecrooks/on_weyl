1e3f379c60865fd409db750f93d8e6087111d2c7
gecrooks <gec@threeplusone.com>
2020-06-03 22:07:51 -0700

    Checkpoint

--- circuits/latex_circuits.py
+++ circuits/latex_circuits.py
@@ -12,15 +12,13 @@ import quantumflow as qf
 
 
 def write_latex(fname, circ):
-    scale = 0.9
-    options = 'thin lines, column sep=0.75em, row sep={2.5em,between origins}'
+    scale = 0.75
     latex = qf.circuit_to_latex(circ, document=False, package='quantikz',
-                                options=options)
+                                qubit_labels=False)
     with open(fname + '.tex', "w") as fout:
-        fout.write(r'\adjustbox{scale=%s}{' % scale)
         fout.write(latex)
-        fout.write('}')
 
+    print('.', end='')
 
 fname = 'cnot'
 circ = qf.Circuit([qf.CNOT(0, 1)])
@@ -47,18 +45,18 @@ fname = 'iswap_to_dcnot'
 circ = qf.Circuit([qf.H(0), qf.S_H(0), qf.S_H(1), qf.ISWAP(1, 0), qf.H(1)])
 write_latex(fname, circ)
 
-fname = 'magic'
-circ = qf.Circuit([qf.Gate(name='M', qubits=[0, 1], tensor=qf.I(0, 1).tensor)])
-write_latex(fname, circ)
+# FIXME
+# fname = 'magic'
+# circ = qf.Circuit([qf.Unitary(name='M', qubits=[0, 1], tensor=qf.I(0, 1).tensor)])
+# write_latex(fname, circ)
 
 fname = 'magic_circ'
 circ = qf.Circuit([qf.S(0), qf.S(1), qf.H(1), qf.CNOT(1, 0)])
 write_latex(fname, circ)
 
-fname = 'qft'
-circ = qf.Circuit([qf.Gate(name='QFT', qubits=[0, 1],
-                           tensor=qf.I(0, 1).tensor)])
-write_latex(fname, circ)
+# fname = 'qft'
+# circ = qf.Circuit([qf.Unitary(qf.IDEN(0, 1).tensor, 0, 1, name='QFT')])
+# write_latex(fname, circ)
 
 fname = 'qft_circ'
 circ = qf.Circuit([qf.SWAP(0, 1), qf.H(1), qf.CZ(0, 1), qf.H(0)])
@@ -70,6 +68,10 @@ write_latex(fname, circ)
 
 fname = 'cv'
 circ = qf.Circuit([qf.CV(0, 1)])
+write_latex(fname, circ)
+
+fname = 'cv_to_cpt'
+circ = qf.Circuit(qf.translate_cv_to_cpt(qf.CV(0, 1)))
 write_latex(fname, circ)
 
 fname = 'cv2'
@@ -152,15 +154,49 @@ fname = 'TZ'
 circ = qf.Circuit([qf.TZ(Symbol('t'))])
 write_latex(fname, circ)
 
-fname = 'ph'
-circ = qf.Circuit([qf.Gate(name='h', qubits=[0], tensor=qf.I(0).tensor)])
+# FIXME
+# fname = 'ph'
+# circ = qf.Circuit([qf.Unitary(name='h', qubits=[0], tensor=qf.I(0).tensor)])
+# write_latex(fname, circ)
+
+# FIXME
+# fname = 'inv_ph'
+# circ = qf.Circuit([qf.Unitary(name=Symbol(r'h$^\dagger$'),
+#                            qubits=[0], tensor=qf.I(0).tensor)])
+# write_latex(fname, circ)
+
+fname = 'ecp'
+circ = qf.Circuit([qf.ECP()])
 write_latex(fname, circ)
 
-fname = 'inv_ph'
-circ = qf.Circuit([qf.Gate(name=Symbol(r'h$^\dagger$'),
-                           qubits=[0], tensor=qf.I(0).tensor)])
+fname = 'ecp_to_sqrtiswap'
+circ = qf.Circuit(qf.translate_ecp_to_sqrtiswap(qf.ECP()))
+write_latex(fname, circ)
+
+fname = 'w_to_ecp'
+circ = qf.Circuit(qf.translate_w_to_ecp(qf.W()))
+write_latex(fname, circ)
+
+fname = 'w_to_ch_cnot'
+circ = qf.Circuit(qf.translate_w_to_ch_cnot(qf.W()))
+write_latex(fname, circ)
+
+fname = 'w_to_cnot'
+circ = qf.Circuit(qf.translate_w_to_cnot(qf.W()))
 write_latex(fname, circ)
 
 
+fname = 'peres'
+circ = qf.Circuit(qf.CCNOT(0, 1, 2), qf.CNOT(0, 1))
+write_latex(fname, circ)
 
+deutsch = qf.Deutsch(Symbol(r'\theta'), 0, 1, 2)
+fname = 'deutsch'
+circ = qf.Circuit(deutsch)
+write_latex(fname, circ)
 
+fname = 'deutsch_to_barenco'
+circ = qf.Circuit(qf.translate_deutsch_to_barenco(deutsch))
+write_latex(fname, circ)
+
+print()
